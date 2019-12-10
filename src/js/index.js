6ce965b588047dc3980f0dbf642c56be95e79d17
Peter Chea <long.chea@nuance.com>
2019-12-10 12:18:42 -0800

    Update html, css, js files to adhere with new webpack configs

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+// With CSS loader, we can import the css in the main JS file, rather than the html file
+import "../css/styles.css"; 
 import { configs } from './key'; 
 
 console.log("Hi, this is our key object: ", configs); 
