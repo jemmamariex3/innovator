407be526d18c527238702b1b2d59e6b28731bc54
DaLarm Han <dalarm.han@nuance.com>
2019-12-11 14:46:05 -0800

    rough draft of credential connection

--- dialog_proto/api.js
+++ dialog_proto/api.js
@@ -3,5 +3,8 @@ var GoogleAuth = require('google-auth-library');
 
 function create_channel(args){
     args = process.argv;
-    var call_credentials = grpc
+    var channel_credentials = grpc.Credentials.createSsl(root_certs);
+    var call_credentials = "omg how do?";
+    channel_credentials = grpc.credentials.combineChannelCredentials(channel_credentials, call_credentials);
+    return channel_credentials;
 }
