dff685c639e585ac0f01fd87207f0184d17d37d1
Peter Chea <long.chea@nuance.com>
2019-12-11 17:18:38 -0800

    Integrate json file with main js file

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,8 @@
 // With CSS loader, we can import the css in the main JS file, rather than the html file
-import "../css/styles.css"; 
-import { configs } from './key'; 
+import "../css/styles.css";
+import { configs } from "./key";
 
-console.log("Hi, this is our key object: ", configs); 
+const token = require("../../dialog_proto/token_scripts/my-token.json");
+
+console.log("Hi, this is our key object: ", configs);
+console.log("Token json: ", token);
