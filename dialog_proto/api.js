2c855031ffc26c8d52cd3dbfb1c408e5ace587c9
DaLarm Han <dalarm.han@nuance.com>
2019-12-12 15:46:14 -0800

    Added in basic ticket and user class into js

--- dialog_proto/api.js
+++ dialog_proto/api.js
@@ -1,7 +1,7 @@
 var grpc = require('grpc');
 var GoogleAuth = require('google-auth-library');
 
-function create_channel(args){
+function create_channel(args) {
     args = process.argv;
     var call_credentials = grpc
 }
