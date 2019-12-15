59bf78c4e819c4507e37b4bc0320ec8bef139084
Peter Chea <long.chea@nuance.com>
2019-12-14 16:49:48 -0800

    Added shell script to run our python api file

---
+++ dialog_proto/google/api/py-script.sh
@@ -0,0 +1,1 @@
+python api-python.py --serverUrl dlgaas.beta.mix.nuance.com:443 --token $MY_TOKEN --modelUrn "urn:nuance:mix/eng-USA/Innovate/mix.dialog" --textInput "Does Kevin have any open tickets?"
