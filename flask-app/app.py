af7d3239da06819bcf42e78409be57e7601ca162
DaLarm Han <dalarm.han@nuance.com>
2019-12-15 22:56:25 -0800

    FINALLY WORKS OMGGG

--- flask-app/app.py
+++ flask-app/app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS
 import sys
 sys.path.append('../google/api')
+print(sys.path)
 from google.api.api_python import *
 import mimetypes
 
@@ -23,8 +24,8 @@ tasks = [
 ]
 
 # API-related info
-modelUrn = "urn:nuance:mix/eng-USA/A174_C517/mix.dialog"
-token = "eyJhbGciOiJSUzI1NiIsImtpZCI6InB1YmxpYzo4MzQ3Zjc3OS1hMDIxLTRlMzEtYTQ4ZC1iNWU1NjdjMzg2ZmMiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOltdLCJjbGllbnRfaWQiOiJhcHBJRDpOTURQVFJJQUxfZGFsYXJtX2hhbl9udWFuY2VfY29tXzIwMTkxMjAyVDE5MjQ1Nzk0MDkzNSIsImV4cCI6MTU3NjQ3ODQxMCwiZXh0Ijp7fSwiaWF0IjoxNTc2NDc0ODEwLCJpc3MiOiJodHRwczovL2F1dGguY3J0Lm51YW5jZS5jb20vIiwianRpIjoiMjU2YTcyOTUtYzQyYy00OTY5LWIzYTQtZjczNjAwYjgxNDMyIiwibmJmIjoxNTc2NDc0ODEwLCJzY3AiOlsiZGxnIl0sInN1YiI6ImFwcElEOk5NRFBUUklBTF9kYWxhcm1faGFuX251YW5jZV9jb21fMjAxOTEyMDJUMTkyNDU3OTQwOTM1In0.VQGpDoT6LpIyC1Hsyx4PHqWGwkB6hpzVgYQ-Kv2-fl16oF6Od_dUK5VUoRnWT8pqiylytoiYiPnnrQqyD854p29Nb0Dfb-Z0tQd9yEepaCZXMQA_sjb_lyz5E93Z530UihaxHT0_iygPnrZHE-QqU1Sf1KsTK1zwdAV_7s5Jxd-8B9VajR-WiTvNdT8WEI66_u7A47mnQQC8xikaJSe8KA3itVBCxyx8708lkXgq5scr2T3YOf4K-qVX7iwmvwzwjnbz61_ZsEy_lXACwLf5klVZSqdv6hqCDzc5jlDDX3jWMRLXaE5CL-vqkOCQqV1G__Cc0p2S_EfN4iCuPgsSoApuf4yZdZu0RJP57PcKfkEN8Fh-KmofRES4qWkAAOckf-Yq9VmlDSGcUXQRLTRTN6mlC6DZ5Wosbj2sd4cJOs9f_ejcrGiCCBjccs6WEnOkdofzUMz21SdZ76K2qSNClHjArqSbaewG1TLtioEjzTFOC9bJwBVDcWQ4suy7WVzjDIhLtuAvPXuSx9qTEADDQndLk2rG8517-pOkU5BHyTb1FntXkUbvQSxuQZILzezap0xdKe9mQvSAAFCk-_2YFs7W3Py2HyoyCHPRuwUijsu005iKgMyxRAeeaL3ryW6Rw644eL3gMBi8qjFlgIL-A9QqIZ1mtkKIaUA3JBWsj4s"
+modelUrn = "urn:nuance:mix/eng-USA/A174_C527/mix.dialog"
+token = "eyJhbGciOiJSUzI1NiIsImtpZCI6InB1YmxpYzo4MzQ3Zjc3OS1hMDIxLTRlMzEtYTQ4ZC1iNWU1NjdjMzg2ZmMiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOltdLCJjbGllbnRfaWQiOiJhcHBJRDpOTURQVFJJQUxfZGFsYXJtX2hhbl9udWFuY2VfY29tXzIwMTkxMjAyVDE5MjQ1Nzk0MDkzNSIsImV4cCI6MTU3NjQ4MjY2NiwiZXh0Ijp7fSwiaWF0IjoxNTc2NDc5MDY2LCJpc3MiOiJodHRwczovL2F1dGguY3J0Lm51YW5jZS5jb20vIiwianRpIjoiMmRlMzZiN2QtNWU5Yy00MzkwLWFmNTAtNWI3Y2NlZGFjNDJkIiwibmJmIjoxNTc2NDc5MDY2LCJzY3AiOlsiZGxnIl0sInN1YiI6ImFwcElEOk5NRFBUUklBTF9kYWxhcm1faGFuX251YW5jZV9jb21fMjAxOTEyMDJUMTkyNDU3OTQwOTM1In0.CeaSiCFfAWhCm7K3PA1tzy-tT-Jrl7idw1Dh43SSssLcbfxVd0oWE1I_ZyeBWUQ8AXDNMmPGAaL4xzdPfNLhAiB9JXYgO43dF27loTVbSHTgxcE8ED5d54DwFJx3FLaZpRczBiQ1RMLGFDR8IiRE2M4vj84X79jL4ZuypCm6DGLS1Vvm8kjczidujazIKgd8REIgnG4UiPIWXKQ85yuV9kmCoRdjZHaL1UUFxfBKozxZIyA0I6xUPgp1E8rleabdwO2UwgVzmGyzR67uP-fXwWbZcT7a7sDKNn02zO89PtwrB6228zcTvDlBFDUzYYxfK2GlBqnsFN2rjoFWKLpD4hJKt6L_1oghKKu_PhFNZovsu5gd9op_gvGjgrh8GfOHPtjALWTMoaOHn6Sc6nKGHrpNolhxtRr0tyVcyKdp0G1DrrnpKCe2wvzOqo62HyuS2Lpy-jTr3maxZSEZR9Ifm1bD8OGmA4GYqApp6MMZolDM98001UeYj70tXqp-Rldol-gNaiHQR_3iY89l15AOMrnOKUn9trdrlACUENlxkprIIMM69t2qTDfChhEuLGGnDB5NgIGjJMeuRvEARwHG5H6HKXBwC5pzzWjxYNLcooy1jTkIz_sHIcJm3KmwoyCMZmMkI7l8v3aGoVQ2oE-bPnv-LqqN9KHJi8Xykb6JpN8"
 serverUrl = "dlgaas.beta.mix.nuance.com:443"
 textInput = "test"
 
@@ -38,6 +39,7 @@ selector_dict = {
 session_id = ''
 
 def start_chat():
+    global session_id
     response, call = start_request(stub, 
                             model_ref=modelUrn, 
                             session_id=str(uuid.uuid1()), # Create a session id
@@ -57,25 +59,26 @@ def start_chat():
                             session_id=session_id, 
                             selector_dict=selector_dict,
                             payload_dict=payload_dict
-                        )    
+                        )  
     va_response = response["payload"]
     message_array = []
     for prompts in va_response["action"]:
         for message in prompts["prompt"]["visual"]:
             message_array.append(message["text"])
-    response, call = stop_request(stub, 
-                            session_id=session_id
-                        )
-    assert call.code() == StatusCode.OK
+    #response, call = stop_request(stub, session_id=session_id)
+    #assert call.code() == StatusCode.OK
     return jsonify(userText=''.join(message_array))
 
 def continue_chat(): 
+    global session_id
     if not request.json:
         abort(400)
     
     payload_dict = {
             "input": request.json
     }
+    print(session_id) 
+    print("hmmmmmmmMMMMMM")
     response, call = execute_request(stub, 
                             session_id=session_id, 
                             selector_dict=selector_dict,
@@ -86,9 +89,6 @@ def continue_chat():
     for prompts in va_response["action"]:
         for message in prompts["prompt"]["visual"]:
             message_array.append(message["text"])
-    response, call = stop_request(stub, 
-                            session_id=session_id
-                        )
     assert call.code() == StatusCode.OK
     return jsonify(userText=''.join(message_array))    
 
