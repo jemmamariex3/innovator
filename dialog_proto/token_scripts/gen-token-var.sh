5bc411ce751f1cafbb23c134e9205dfa4204c5b7
DaLarm Han <dalarm.han@nuance.com>
2019-12-20 01:21:14 -0800

    Working api crud

--- dialog_proto/token_scripts/gen-token-var.sh
+++ dialog_proto/token_scripts/gen-token-var.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CLIENT_ID="appID%3ANMDPTRIAL_dalarm_han_nuance_com_20191202T192457940935"
-SECRET="mJ7iKz81mK724Ufic73d_0x81h9_B_nNhZKlwVSqT5k"
+SECRET="zvqtKbRmXMVH9Di9EwXJpFOHZfDFQufmR6XcvXu4IXk"
 curl -s -u "$CLIENT_ID:$SECRET" "https://auth.crt.nuance.com/oauth2/token" \
 -d 'grant_type=client_credentials' -d 'scope=dlg' \
 | python -c 'import os, sys, json; print(json.load(sys.stdin)["access_token"])' \
