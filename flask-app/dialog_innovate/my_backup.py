0158a87cdc5d19cf2fa42e6e7461899a64ef7590
DaLarm Han <dalarm.han@nuance.com>
2019-12-19 22:11:57 -0800

    updated array for tickets

--- flask-app/dialog_innovate/my_backup.py
+++ flask-app/dialog_innovate/my_backup.py
@@ -41,9 +41,234 @@ selector_dict = {
 #textInput = "test"  Use this if you want to hardcode messages.
 
 ticketsArr = [
-    { 'number': 1, 'assignee': "DaLarm Han", 'createdDate': "12/17/2019", 'startDate': "12/18/2019", 'endDate': "12/19/2019", 'client': 'Verizon', 'summary': "Register team in mix", 'hoursLogged': 8, 'priority': "high", 'status': 'open', 'comments': "Ready in prod"},
-    { 'number': 2, 'assignee': "DaLarm Han", 'createdDate': "12/17/2019", 'startDate': "12/18/2019", 'endDate': "12/19/2019", 'client': 'AT&T', 'summary': "Register teamz in mix", 'hoursLogged': 2, 'priority': "low", 'status': 'open', 'comments': "Ready in prod"},
-    { 'number': 3, 'assignee': "DaLarm Han", 'createdDate': "12/17/2019", 'startDate': "12/18/2019", 'endDate': "12/19/2019", 'client': 'Sprint', 'summary': "Register teamzzz in mix", 'hoursLogged': 3, 'priority': "medium", 'status': 'open', 'comments': "Ready in prod"}
+    {
+        "number": "10001",
+        "summary": "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        "client": "Verizon",
+        "priority": "Low",
+        "status": "Open",
+        "startDate": "12-13-19",
+        "endDate": "12-15-19",
+        "assignee": "DaLarm",
+        "hoursLogged": "0",
+        "comments": "Lacus vestibulum sed arcu non odio."
+    },
+    {
+        "number": "10002",
+        "summary": "Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "client": "Sprint",
+        "priority": "Medium",
+        "status": "In Progress",
+        "startDate": "12-14-19",
+        "endDate": "12-16-19",
+        "assignee": "Peter",
+        "hoursLogged": "1",
+        "comments": "Sociis natoque penatibus et magnis dis parturient montes nascetur. "
+    },
+    {
+        "number": "10003",
+        "summary": "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
+        "client": "ATT",
+        "priority": "High",
+        "status": "Complete",
+        "startDate": "12-15-19",
+        "endDate": "12-17-19",
+        "assignee": "Jemma",
+        "hoursLogged": "2",
+        "comments": "Nisi lacus sed viverra tellus in. Convallis posuere morbi leo urna molestie at elementum eu."
+    },
+    {
+        "number": "10004",
+        "summary": "Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "client": "Costco",
+        "priority": "Low",
+        "status": "Closed",
+        "startDate": "12-16-19",
+        "endDate": "12-18-19",
+        "assignee": "OK",
+        "hoursLogged": "2.5",
+        "comments": "At auctor urna nunc id cursus. "
+    },
+    {
+        "number": "10005",
+        "summary": "Integer enim neque volutpat ac tincidunt.",
+        "client": "Verizon",
+        "priority": "Medium",
+        "status": "Open",
+        "startDate": "12-17-19",
+        "endDate": "12-19-19",
+        "assignee": "NK",
+        "hoursLogged": "0",
+        "comments": "Egestas erat imperdiet sed euismod."
+    },
+    {
+        "number": "10006",
+        "summary": "Aliquam faucibus purus in massa tempor.",
+        "client": "Sprint",
+        "priority": "High",
+        "status": "In Progress",
+        "startDate": "12-18-19",
+        "endDate": "12-20-19",
+        "assignee": "DaLarm",
+        "hoursLogged": "1",
+        "comments": "Est lorem ipsum dolor sit amet consectetur."
+    },
+    {
+        "number": "10007",
+        "summary": "Lacus vestibulum sed arcu non odio.",
+        "client": "ATT",
+        "priority": "Low",
+        "status": "Complete",
+        "startDate": "12-19-19",
+        "endDate": "12-21-19",
+        "assignee": "Peter",
+        "hoursLogged": "2",
+        "comments": "Arcu vitae elementum curabitur vitae nunc sed velit."
+    },
+    {
+        "number": "10008",
+        "summary": "Sociis natoque penatibus et magnis dis parturient montes nascetur. ",
+        "client": "Costco",
+        "priority": "Medium",
+        "status": "Closed",
+        "startDate": "12-20-19",
+        "endDate": "12-22-19",
+        "assignee": "Jemma",
+        "hoursLogged": "2.5",
+        "comments": "Libero justo laoreet sit amet cursus sit amet dictum sit."
+    },
+    {
+        "number": "10009",
+        "summary": "Nisi lacus sed viverra tellus in. Convallis posuere morbi leo urna molestie at elementum eu.",
+        "client": "Verizon",
+        "priority": "High",
+        "status": "Open",
+        "startDate": "12-21-19",
+        "endDate": "12-23-19",
+        "assignee": "OK",
+        "hoursLogged": "0",
+        "comments": "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+    },
+    {
+        "number": "10010",
+        "summary": "At auctor urna nunc id cursus. ",
+        "client": "Sprint",
+        "priority": "Low",
+        "status": "In Progress",
+        "startDate": "12-22-19",
+        "endDate": "12-24-19",
+        "assignee": "NK",
+        "hoursLogged": "1",
+        "comments": "Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+        "number": "10011",
+        "summary": "Egestas erat imperdiet sed euismod.",
+        "client": "ATT",
+        "priority": "Medium",
+        "status": "Complete",
+        "startDate": "12-23-19",
+        "endDate": "12-25-19",
+        "assignee": "DaLarm",
+        "hoursLogged": "2",
+        "comments": "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "
+    },
+    {
+        "number": "10012",
+        "summary": "Est lorem ipsum dolor sit amet consectetur.",
+        "client": "Costco",
+        "priority": "High",
+        "status": "Closed",
+        "startDate": "12-24-19",
+        "endDate": "12-26-19",
+        "assignee": "Peter",
+        "hoursLogged": "2.5",
+        "comments": "Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        "number": "10013",
+        "summary": "Arcu vitae elementum curabitur vitae nunc sed velit.",
+        "client": "Verizon",
+        "priority": "Low",
+        "status": "Open",
+        "startDate": "12-25-19",
+        "endDate": "12-27-19",
+        "assignee": "Jemma",
+        "hoursLogged": "0",
+        "comments": "Integer enim neque volutpat ac tincidunt."
+    },
+    {
+        "number": "10014",
+        "summary": "Libero justo laoreet sit amet cursus sit amet dictum sit.",
+        "client": "Sprint",
+        "priority": "Medium",
+        "status": "In Progress",
+        "startDate": "12-26-19",
+        "endDate": "12-28-19",
+        "assignee": "OK",
+        "hoursLogged": "1",
+        "comments": "Aliquam faucibus purus in massa tempor."
+    },
+    {
+        "number": "10015",
+        "summary": "Augue eget arcu dictum varius duis at consectetur lorem.",
+        "client": "ATT",
+        "priority": "High",
+        "status": "Complete",
+        "startDate": "12-27-19",
+        "endDate": "12-29-19",
+        "assignee": "NK",
+        "hoursLogged": "2",
+        "comments": "Augue eget arcu dictum varius duis at consectetur lorem."
+    },
+    {
+        "number": "10016",
+        "summary": "A erat nam at lectus urna. ",
+        "client": "Costco",
+        "priority": "Low",
+        "status": "Closed",
+        "startDate": "12-28-19",
+        "endDate": "12-30-19",
+        "assignee": "DaLarm",
+        "hoursLogged": "2.5",
+        "comments": "A erat nam at lectus urna. "
+    },
+    {
+        "number": "10017",
+        "summary": "Nulla pharetra diam sit amet nisl. ",
+        "client": "Verizon",
+        "priority": "Medium",
+        "status": "Open",
+        "startDate": "12-29-19",
+        "endDate": "12-31-19",
+        "assignee": "Peter",
+        "hoursLogged": "0",
+        "comments": "Nulla pharetra diam sit amet nisl. "
+    },
+    {
+        "number": "10018",
+        "summary": "Massa vitae tortor condimentum lacinia quis vel eros donec.",
+        "client": "Sprint",
+        "priority": "High",
+        "status": "In Progress",
+        "startDate": "12-30-19",
+        "endDate": "01-01-20",
+        "assignee": "Jemma",
+        "hoursLogged": "1",
+        "comments": "Massa vitae tortor condimentum lacinia quis vel eros donec."
+    },
+    {
+        "number": "10019",
+        "summary": "Pellentesque diam volutpat commodo sed egestas.",
+        "client": "ATT",
+        "priority": "High",
+        "status": "Complete",
+        "startDate": "12-31-19",
+        "endDate": "01-02-20",
+        "assignee": "OK",
+        "hoursLogged": "2",
+        "comments": "Pellentesque diam volutpat commodo sed egestas."
+    }
 ]
 response_ticketsArr = []
 
